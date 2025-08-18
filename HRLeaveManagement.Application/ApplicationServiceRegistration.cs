ed8ab0a35e25de5646c064bc7a1c2690ede379a2
shamsisadr01 <shamsisadr01@gmail.com>
2025-08-19 00:15:38 +0330

    Add AutoMapper and MediatR for LeaveType features
    Updated project file to include AutoMapper and MediatR packages.
    Created ApplicationServiceRegistration for service configuration.
    Added LeaveTypeDto for data transfer and LeaveTypeProfile for mapping.

---
+++ HRLeaveManagement.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using HRLeaveManagement.Application.MappingProfiles;
+
+namespace HRLeaveManagement.Application;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddAutoMapper(options =>
+        {
+            options.AddProfile<LeaveTypeProfile>();
+        });
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        return services;
+    }
+}
---
+++ HRLeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using HRLeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using HRLeaveManagement.Domain;
+
+namespace HRLeaveManagement.Application.MappingProfiles;
+
+public class LeaveTypeProfile : Profile
+{
+    public LeaveTypeProfile()
+    {
+        CreateMap<LeaveTypeDto, LeaveType>().ReverseMap();
+    }
+}
