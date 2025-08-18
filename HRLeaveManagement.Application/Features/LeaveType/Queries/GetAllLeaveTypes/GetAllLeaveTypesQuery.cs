e25cc805db2128097b92c62a94d9ef82e4985cb4
shamsisadr01 <shamsisadr01@gmail.com>
2025-08-19 00:35:15 +0330

    Add query and handler for retrieving all leave types
    Introduces `GetAllLeaveTypesQuery` and `GetAllLeaveTypesQueryHandler` to handle requests for fetching all leave types. The handler uses `ILeaveTypeRepository` for asynchronous data retrieval and `IMapper` for mapping to the domain model, enhancing the application's querying capabilities.

---
+++ HRLeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetAllLeaveTypesQuery.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using HRLeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HRLeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+public record GetAllLeaveTypesQuery:IRequest<IEnumerable<Domain.LeaveType>>;
+
+
+public class GetAllLeaveTypesQueryHandler : IRequestHandler<GetAllLeaveTypesQuery, IEnumerable<Domain.LeaveType>>
+{
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+    private readonly IMapper _mapper;
+
+    public GetAllLeaveTypesQueryHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+    {
+        _leaveTypeRepository = leaveTypeRepository;
+        _mapper = mapper;
+    }
+    public async Task<IEnumerable<Domain.LeaveType>> Handle(GetAllLeaveTypesQuery request, CancellationToken cancellationToken)
+    {
+        var leaveTypes = await _leaveTypeRepository.GetAllAsync();
+
+        var data = _mapper.Map<IEnumerable<Domain.LeaveType>>(leaveTypes);
+
+        return data;
+    }
+}
