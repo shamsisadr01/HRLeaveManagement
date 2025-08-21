76fe7491d8b4ea9a99da0ab82ad728a2db95b4dd
shamsisadr01 <shamsisadr01@gmail.com>
2025-08-21 22:21:45 +0330

    Unit Testing the Application Code - Setup Mocks

---
+++ HRLeaveManagement.Applications.UnitTests/Mocks/MockLeaveTypeRepository.cs
@@ -0,0 +1,41 @@
+﻿using HRLeaveManagement.Application.Contracts.Persistence;
+using HRLeaveManagement.Domain;
+using Moq;
+
+namespace HRLeaveManagement.Applications.UnitTests.Mocks;
+
+public class MockLeaveTypeRepository
+{
+    public static Mock<ILeaveTypeRepository> GetLeaveTypesMockLeaveTypeRepository()
+    {
+        var leaveTypes = new List<LeaveType>
+        {
+            new LeaveType
+            {
+                Id = 1,
+                Name = "Annual Leave",
+                DefaultDays = 20,
+            },
+            new LeaveType
+            {
+                Id = 2,
+                Name = "Sick Leave",
+                DefaultDays = 10,
+            }
+        };
+
+        var mock = new Mock<ILeaveTypeRepository>();
+
+        mock.Setup(repo => repo.GetAllAsync())
+            .ReturnsAsync(leaveTypes);
+
+        mock.Setup(repo => repo.AddAsync(It.IsAny<LeaveType>()))
+            .Returns((LeaveType leaveType) =>
+            {
+                leaveTypes.Add(leaveType);
+                return Task.CompletedTask;
+            });
+
+        return mock;
+    }
+}
