1a0c52b88b602e9a6bfcff65767de9c896146e37
shamsisadr01 <shamsisadr01@gmail.com>
2025-08-23 12:28:06 +0330

    Setup Leave Allocation Management

--- HRLeaveManagement.Application/Features/LeaveAllocation/Commands/CreateLeaveAllocation/CreateLeaveAllocationCommandHandler.cs
+++ HRLeaveManagement.Application/Features/LeaveAllocation/Commands/CreateLeaveAllocation/CreateLeaveAllocationCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using HRLeaveManagement.Application.Contracts.Identity;
 using HRLeaveManagement.Application.Contracts.Persistence;
 using HRLeaveManagement.Application.Exceptions;
 using MediatR;
@@ -9,13 +10,15 @@ public class CreateLeaveAllocationCommandHandler : IRequestHandler<CreateLeaveAl
 {
     private readonly ILeaveAllocationRepository _leaveAllocationRepository;
     private readonly ILeaveTypeRepository _leaveTypeRepository;
+    private readonly IUserService _userService;
     private readonly IMapper _mapper;
 
-    public CreateLeaveAllocationCommandHandler(ILeaveAllocationRepository leaveAllocationRepository, IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+    public CreateLeaveAllocationCommandHandler(ILeaveAllocationRepository leaveAllocationRepository, IMapper mapper, ILeaveTypeRepository leaveTypeRepository, IUserService userService)
     {
         _leaveAllocationRepository = leaveAllocationRepository;
         _mapper = mapper;
         _leaveTypeRepository = leaveTypeRepository;
+        _userService = userService;
     }
 
     public async Task<Unit> Handle(CreateLeaveAllocationCommand request, CancellationToken cancellationToken)
@@ -28,10 +31,36 @@ public class CreateLeaveAllocationCommandHandler : IRequestHandler<CreateLeaveAl
             throw new BadRequestExceptions("Invalid Leave Allocation Request", validationResult);
         }
 
-        var leaveType = _leaveTypeRepository.GetByIdAsync(request.LeaveTypeId);
+        var leaveType = await _leaveTypeRepository.GetByIdAsync(request.LeaveTypeId);
 
-        var leaveAllocation = _mapper.Map<Domain.LeaveAllocation>(request);
-        await _leaveAllocationRepository.AddAsync(leaveAllocation);
+        var employees = await _userService.GetEmployeesAsync();
+
+        var period = DateTime.Now.Year;
+
+        var allocations = new List<Domain.LeaveAllocation>();
+
+        foreach (var employee in employees)
+        {
+            var allocationExit = await _leaveAllocationRepository.AllocationExists(request.LeaveTypeId, employee.Id.ToString(),period);
+            if (allocationExit == false)
+            {
+                allocations.Add(new Domain.LeaveAllocation()
+                {
+                    Id = employee.Id,
+                    LeaveTypeId = request.LeaveTypeId,
+                    NumberOfDays = leaveType.DefaultDays,
+                    Period = period,
+                });
+            }
+        }
+
+        if (allocations.Any())
+        {
+            await _leaveAllocationRepository.AddAllocations(allocations);
+        }
+
+      //  var leaveAllocation = _mapper.Map<Domain.LeaveAllocation>(request);
+      //  await _leaveAllocationRepository.AddAsync(leaveAllocation);
 
         return Unit.Value;
     }
--- HRLeaveManagement.Mvc.UI/Services/LeaveAllocationService.cs
+++ HRLeaveManagement.Mvc.UI/Services/LeaveAllocationService.cs
@@ -8,5 +8,23 @@ namespace HRLeaveManagement.Mvc.UI.Services
         public LeaveAllocationService(IClient client) : base(client)
         {
         }
+
+        public async Task<Response<Guid>> CreateLeaveAllocations(int leaveTypeId)
+        {
+            try
+            {
+                var response = new Response<Guid>();
+                CreateLeaveAllocationCommand createLeaveAllocation = new() { LeaveTypeId = leaveTypeId };
+
+                await _client.LeaveAllocationsPOSTAsync(createLeaveAllocation);
+                return response;
+            }
+            catch (ApiException ex)
+            {
+                return ConvertApiExceptions(ex);
+            }
+        }
+
+   
     }
 }
